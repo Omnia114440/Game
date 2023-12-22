907f79f8befa7fe1dce5cb2399157e93905dc9ef
omnia <omnia@gmail.com>
2023-12-22 23:20:15 +0200

    this is sum

---
+++ newfile.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int my_sum(int a , int b , int c);
+
+int main()
+{
+  cout << "dear student!\n";
+  cout << "lets blah blah";
+  cout << my_sum(2,4,6);
+
+}
+
+
+int my_sum(int a ,int b, int c)
+{
+
+return a+b+c;
+{
